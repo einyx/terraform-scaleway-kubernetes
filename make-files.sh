0cda3d97e0f5cd98dc2db440d4c81f8ea030027f
Alessio <alessio@linux.com>
2020-03-05 16:09:06 +0100

    Initial fork commit

---
+++ make-files.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+. ./ips.txt
+cat > scw-install.sh << FIN
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+
+EOF
+
+echo "DOCKER_OPTS='-H unix:///var/run/docker.sock --label provider=scaleway --mtu=1500" > /etc/default/docker
+systemctl restart docker
+
+apt-get update -qq \
+ && apt install docker.io \
+ && apt dist-upgrade -y
+ && apt install -y -q --no-install-recommends kubelet kubeadm kubectl kubernetes-cni \
+ && apt clean
+
+for arg in "$@"
+do
+  case $arg in
+    'master')
+      SUID=$(scw-metadata --cached ID)
+      PUBLIC_IP=$(scw-metadata --cached PUBLIC_IP_ADDRESS)
+      PRIVATE_IP=$(scw-metadata --cached PRIVATE_IP)
+
+      kubeadm --token=$KUBERNETES_TOKEN --apiserver-advertise-address=$PUBLIC_IP --service-dns-domain=$SUID.pub.cloud.scaleway.com init --pod-network-cidr=192.168.0.0/16 
+
+      KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
+      break
+      ;;
+    'slave')
+      kubeadm join --token $KUBERNETES_TOKEN $MASTER_00:6443
+      break
+      ;;
+ esac
+done
+
+FIN
+rm -rf ./ips.txt
